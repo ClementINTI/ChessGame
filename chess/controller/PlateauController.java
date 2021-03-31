7f2658b38c6c6a2318b9c27f33ef0ed1be04f00f
Nicolas Rondet <rondet.nicolas@gmail.com>
2021-03-31 14:06:45 +0200

    Début de controller

--- chess/controller/PlateauController.java
+++ chess/controller/PlateauController.java
@@ -1,5 +1,34 @@
 package chess.controller;
 
-public class PlateauController {
-	
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PlateauController implements Initializable {
+
+    @FXML
+    private Button buttonNewGame;
+
+    @FXML
+    private Button buttonQuit;
+
+    @FXML
+    private Button buttonUndo;
+
+    @FXML
+    private Label labelPlayerTurn;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
+
+
+
+    }
 }
