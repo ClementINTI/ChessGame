e15cddf27addc6f6b652575e1cdd28d05699788b
clement wa <clement.wasselet@laposte.net>
2021-04-01 10:58:08 +0200

    Case du controleur dans une liste

--- chess/controller/PlateauController.java
+++ chess/controller/PlateauController.java
@@ -1,6 +1,9 @@
 package chess.controller;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import chess.model.Partie;
@@ -52,6 +55,7 @@ public class PlateauController implements Initializable {
 	private AnchorPane colonne0ligne7, colonne1ligne7, colonne2ligne7, colonne3ligne7, colonne4ligne7, colonne5ligne7,
 			colonne6ligne7, colonne7ligne7;
 
+	private List<AnchorPane> listeCasesFXML = new ArrayList<AnchorPane>();
 	// faire la classe
 	private Partie partie;
 
@@ -64,7 +68,18 @@ public class PlateauController implements Initializable {
 		buttonNewGame.setOnMouseClicked(event -> debutNouvellePartie());
 		buttonQuit.setOnMouseClicked(event -> System.exit(0));
 		buttonUndo.setOnMouseClicked(event -> annuler());
-
+		// listeCasesFXML.addAll(Collections.addAll(c, elements));
+		listeCasesFXML.addAll(Arrays.asList(colonne0ligne0, colonne1ligne0, colonne2ligne0, colonne3ligne0,
+				colonne4ligne0, colonne5ligne0, colonne6ligne0, colonne7ligne0, colonne0ligne1, colonne1ligne1,
+				colonne2ligne1, colonne3ligne1, colonne4ligne1, colonne5ligne1, colonne6ligne1, colonne7ligne1,
+				colonne0ligne2, colonne1ligne2, colonne2ligne2, colonne3ligne2, colonne4ligne2, colonne5ligne2,
+				colonne6ligne2, colonne7ligne2, colonne0ligne3, colonne1ligne3, colonne2ligne3, colonne3ligne3,
+				colonne4ligne3, colonne5ligne3, colonne6ligne3, colonne7ligne3, colonne0ligne4, colonne1ligne4,
+				colonne2ligne4, colonne3ligne4, colonne4ligne4, colonne5ligne4, colonne6ligne4, colonne7ligne4,
+				colonne0ligne5, colonne1ligne5, colonne2ligne5, colonne3ligne5, colonne4ligne5, colonne5ligne5,
+				colonne6ligne5, colonne7ligne5, colonne0ligne6, colonne1ligne6, colonne2ligne6, colonne3ligne6,
+				colonne4ligne6, colonne5ligne6, colonne6ligne6, colonne7ligne6, colonne0ligne7, colonne1ligne7,
+				colonne2ligne7, colonne3ligne7, colonne4ligne7, colonne5ligne7, colonne6ligne7, colonne7ligne7));
 		// colonne0ligne0.setOnMouseClicked();
 
 	}
