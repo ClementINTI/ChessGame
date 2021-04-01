bebdd6360905d09f1c1f77d1de9e1ce30ce9865e
Nicolas Rondet <rondet.nicolas@gmail.com>
2021-04-01 16:02:37 +0200

    on y est presque

--- chess/model/pieces/Piece.java
+++ chess/model/pieces/Piece.java
@@ -2,10 +2,12 @@ package chess.model.pieces;
 
 import java.awt.Image;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 import chess.model.Case;
 import chess.model.Deplacement;
+import chess.model.Partie;
 import chess.model.Plateau;
 
 public class Piece {
@@ -45,8 +47,15 @@ public class Piece {
 	 * @return
 	 */
 	public List<Deplacement> deplacer(Plateau plateau, Case emplacement) {
-		// plateau.getListeCase();
-		return null;
+		List<Case> listeCase  = plateau.getListeCase();
+		List<Deplacement> deplacements = new ArrayList<>();
+		for (int i = 0; i < 64; i++) {
+				if (emplacement != listeCase.get(i)) {
+					deplacements.add(new Deplacement(emplacement, listeCase.get(i)));
+				}
+		}
+	return deplacements;
+
 
 	}
 
