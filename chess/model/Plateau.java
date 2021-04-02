038055d8c097abe815cb32bef8a243844a8db2b3
clement wa <clement.wasselet@laposte.net>
2021-04-02 14:53:00 +0200

    Ménage du code Plateau et début des déplacement du cavaliers

--- chess/model/Plateau.java
+++ chess/model/Plateau.java
@@ -12,20 +12,6 @@ import chess.model.pieces.Roi;
 import chess.model.pieces.Tour;
 
 public class Plateau {
-	private Case colonne0ligne0, colonne1ligne0, colonne2ligne0, colonne3ligne0, colonne4ligne0, colonne5ligne0,
-			colonne6ligne0, colonne7ligne0;
-	private Case colonne0ligne2, colonne1ligne2, colonne2ligne2, colonne3ligne2, colonne4ligne2, colonne5ligne2,
-			colonne6ligne2, colonne7ligne2;
-	private Case colonne0ligne3, colonne1ligne3, colonne2ligne3, colonne3ligne3, colonne4ligne3, colonne5ligne3,
-			colonne6ligne3, colonne7ligne3;
-	private Case colonne0ligne4, colonne1ligne4, colonne2ligne4, colonne3ligne4, colonne4ligne4, colonne5ligne4,
-			colonne6ligne4, colonne7ligne4;
-	private Case colonne0ligne5, colonne1ligne5, colonne2ligne5, colonne3ligne5, colonne4ligne5, colonne5ligne5,
-			colonne6ligne5, colonne7ligne5;
-	private Case colonne0ligne6, colonne1ligne6, colonne2ligne6, colonne3ligne6, colonne4ligne6, colonne5ligne6,
-			colonne6ligne6, colonne7ligne6;
-	private Case colonne0ligne7, colonne1ligne7, colonne2ligne7, colonne3ligne7, colonne4ligne7, colonne5ligne7,
-			colonne6ligne7, colonne7ligne7;
 
 	private List<Piece> piecesNoir = new ArrayList<Piece>();
 	private List<Piece> piecesBlanc = new ArrayList<Piece>();
--- chess/model/pieces/Cavalier.java
+++ chess/model/pieces/Cavalier.java
@@ -1,6 +1,5 @@
 package chess.model.pieces;
 
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,24 +20,46 @@ public class Cavalier extends Piece {
 
 	@Override
 	public String getImage() {
-		return "/chess/piecesImage/white_knight.png";
+		if (isBlanc()) {
+			return "/chess/piecesImage/white_knight.png";
+		} else {
+			return "/chess/piecesImage/black_knight.png";
+		}
 	}
 
 	@Override
 	public List<Deplacement> deplacer(Plateau plateau, Case emplacement) {
-		List<Case> listeCase  = plateau.getListeCase();
+		List<Case> listeCase = plateau.getListeCase();
 		List<Deplacement> deplacements = new ArrayList<>();
-		for (int i = 0; i < 64; i++) {
-			if (emplacement != listeCase.get(i)) {
-				if (listeCase.get(i).getPiece() != null) {
-					if (listeCase.get(i).getPiece().isBlanc() != emplacement.getPiece().isBlanc()) {
-						deplacements.add(new Deplacement(emplacement, listeCase.get(i)));
-					}
-				} else {
-					deplacements.add(new Deplacement(emplacement, listeCase.get(i)));
-				}
+		int indexActuelle = listeCase.indexOf(emplacement);
+
+		deplacementPossible(deplacements, indexActuelle + 6, emplacement, listeCase);
+		deplacementPossible(deplacements, indexActuelle + 15, emplacement, listeCase);
+		deplacementPossible(deplacements, indexActuelle + 10, emplacement, listeCase);
+		deplacementPossible(deplacements, indexActuelle + 17, emplacement, listeCase);
+		deplacementPossible(deplacements, indexActuelle - 6, emplacement, listeCase);
+		deplacementPossible(deplacements, indexActuelle - 15, emplacement, listeCase);
+		deplacementPossible(deplacements, indexActuelle - 10, emplacement, listeCase);
+		deplacementPossible(deplacements, indexActuelle - 17, emplacement, listeCase);
+
+		/**
+		 * for (int i = 0; i < 64; i++) { Case emplacementPossible=listeCase.get(i); if
+		 * (emplacementPossible.getPiece() != null){ if (emplacement !=
+		 * emplacementPossible) { if (emplacementPossible.getPiece().isBlanc() !=
+		 * emplacement.getPiece().isBlanc()) { deplacements.add(new
+		 * Deplacement(emplacement, emplacementPossible)); } } else {
+		 * deplacements.add(new Deplacement(emplacement, emplacementPossible)); } } }
+		 */
+		return deplacements;
+	}
+
+	private void deplacementPossible(List<Deplacement> deplacements, int indexPossible, Case emplacement,
+			List<Case> listeCase) {
+		if (indexPossible < 64) {
+			Case casePossible = listeCase.get(indexPossible);
+			if (casePossible.getPiece().isBlanc() != isBlanc()) {
+				deplacements.add(new Deplacement(emplacement, listeCase.get(indexPossible)));
 			}
 		}
-		return deplacements;
 	}
 }
